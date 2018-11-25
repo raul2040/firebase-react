fb6c601344f4884f9966a3b6ff7a383ebc247f7c
Raúl <raul2040@outlook.es>
2018-11-25 10:51:55 +0100

    Añadida la autentificación con google

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import firebase from 'firebase';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            user: null
+        };
+        this.renderLoginButton = this.renderLoginButton.bind(this);
+    }
+    componentWillMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            this.setState({user});
+        });
+    }
+
+    handleAuth() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth().signInWithPopup(provider)
+            .then(result => console.log(`${result.user.email} ha inciado sesión`))
+            .catch(error => console.log(`Error: ${error.code}: ${error.message}`))
+    }
+    renderLoginButton() {
+        if(this.state.user) {
+            return(
+                <div>
+                    <img src={this.state.user.photoURL} alt={this.state.user.displayName} />
+                    <p>Hola {this.state.user.displayName}!</p>
+                </div>
+            )
+        }
+        else{
+            return (<button onClick={this.handleAuth}> Login con Google </button>)
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h2>Proyecto despliegue de aplicaciones web</h2>
+                </header>
+                <p className={"App-intro"}>
+                    {this.renderLoginButton()}
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import firebase from 'firebase';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+firebase.initializeApp({
+    apiKey: "AIzaSyD-l23KQDtAZKJ4xQoeAYYVnGmWzMQXodQ",
+    authDomain: "fir-con-react.firebaseapp.com",
+    databaseURL: "https://fir-con-react.firebaseio.com",
+    projectId: "fir-con-react",
+    storageBucket: "fir-con-react.appspot.com",
+    messagingSenderId: "16039068622"
+});
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
